59425f32890f42ebe7e3acce446304686ee46420
marouaneMJH <marwanmoujahid008@gmail.com>
2025-10-05 16:53:30 +0100

    work to print the token in out-file

--- src/main.c
+++ src/main.c
@@ -1,31 +1,52 @@
 #include <stdio.h>
 #include "scanner.h"
 
-#define FILE_PATH "./asset/file.txt"
+#define CHECK_FILE(file, path)                     \
+    if (file == NULL)                              \
+    {                                              \
+        printf("Could not open file: %s\n", path); \
+        return 1;                                  \
+    }
+
+#define IN_FILE_PATH "./asset/file.txt"
+#define OUT_FILE_PATH "./asset/out_file.txt"
 
 int main()
 {
 
-    FILE *file = fopen(FILE_PATH, "r");
+    FILE *file = fopen(IN_FILE_PATH, "r");
+    FILE *out_file = fopen(OUT_FILE_PATH, "w");
 
-    if (file == NULL)
-    {
-        printf("Could not open file %s for reading\n", FILE_PATH);
-        return 1;
-    }
+    CHECK_FILE(file, IN_FILE_PATH);
+    CHECK_FILE(out_file, OUT_FILE_PATH);
+
     int i = 0;
     while (1)
     {
         i++;
         Token t = scanner(file);
         const char *token_names[] = {
-            "BEGIN", "END", "READ", "WRITE", "ID",
-            "INT_LITERAL", "SCAN_OF", "PLUS_OP", "MINUS_OP",
-            "SCANEOF", "L_PAREN", "R_PAREN", "SEMICOLON",
-            "COMMA", "ASSIGN_OP"
-            // Add all other token types from your enum
+
+            "BEGIN",
+            "END",
+            "READ",
+            "WRITE",
+            "ID",
+            "INT_LITERAL",
+            "CURLY_BRACE_OPEN",
+            "CURLY_BRACE_CLOSE",
+            "SCAN_OF",
+            "PLUS_OP",
+            "MINUS_OP",
+            "SCANE_OF",
+            "L_PAREN",
+            "R_PAREN",
+            "SEMICOLON",
+            "COMMA",
+            "ASSIGN_OP",
         };
-        printf("Token: %s, Lexeme: %s\n", token_names[t], "NONE FOR NOW");
+
+        fprintf(out_file, "%s ", token_names[t]);
 
         if (i > 20)
             break;
